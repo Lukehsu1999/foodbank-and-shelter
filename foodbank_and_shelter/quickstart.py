30f4c42d4b1e716932af5650c76935a16ae4c1ba
Lukehsu <lukehsu1999@gmail.com>
2024-04-08 21:28:57 -0400

    add conversation chain

--- foodbank_and_shelter/quickstart.py
+++ foodbank_and_shelter/quickstart.py
@@ -11,6 +11,10 @@ from langchain_text_splitters import RecursiveCharacterTextSplitter
 from langchain.chains.combine_documents import create_stuff_documents_chain
 from langchain_core.documents import Document
 from langchain.chains import create_retrieval_chain
+# for chatbot
+from langchain.chains import create_history_aware_retriever
+from langchain_core.prompts import MessagesPlaceholder
+from langchain_core.messages import HumanMessage, AIMessage
 
 load_dotenv()
 LANGCHAIN_TRACING_V2 = os.getenv('LANGCHAIN_TRACING_V2')
@@ -34,7 +38,7 @@ chain = prompt | llm | output_parser
 
 #print(chain.invoke({"input": "how can langsmith help with testing?"}))
 
-# set up retriever
+# Part 2: set up retriever
 # if we are fetching documents from web
 loader = WebBaseLoader("https://docs.smith.langchain.com/user_guide")
 docs = loader.load()
@@ -69,3 +73,32 @@ retriever = vector.as_retriever()
 retrieval_chain = create_retrieval_chain(retriever, document_chain)
 response = retrieval_chain.invoke({"input": "how can langsmith help with testing?"})
 print(response["answer"])
+
+# Part 3: set up chatbot
+prompt = ChatPromptTemplate.from_messages([
+    MessagesPlaceholder(variable_name="chat_history"),
+    ("user", "{input}"),
+    ("user", "Given the above conversation, generate a search query to look up to get information relevant to the conversation")
+])
+retriever_chain = create_history_aware_retriever(llm, retriever, prompt)
+
+chat_history = [HumanMessage(content="Can LangSmith help test my LLM applications?"), AIMessage(content="Yes!")]
+print(retriever_chain.invoke({
+    "chat_history": chat_history,
+    "input": "Tell me how"
+}))
+
+prompt = ChatPromptTemplate.from_messages([
+    ("system", "Answer the user's questions based on the below context:\n\n{context}"),
+    MessagesPlaceholder(variable_name="chat_history"),
+    ("user", "{input}"),
+])
+document_chain = create_stuff_documents_chain(llm, prompt)
+
+retrieval_chain = create_retrieval_chain(retriever_chain, document_chain)
+
+chat_history = [HumanMessage(content="Can LangSmith help test my LLM applications?"), AIMessage(content="Yes!")]
+retrieval_chain.invoke({
+    "chat_history": chat_history,
+    "input": "Tell me how"
+})
